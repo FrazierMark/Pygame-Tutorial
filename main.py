db16e7f8cfa6f5619e62a0bd33a74d051269d125
FrazierMark <m.mark.frazier@gmail.com>
2023-11-08 14:35:15 -0800

    Alien/bomb create and randomly changes direction

--- main.py
+++ main.py
@@ -61,6 +61,41 @@ class Strawberry(GameObject):
     self.y = choice(lanes)
     self.x = choice([-64, 500])
 
+class Bomb(GameObject):
+  def __init__(self):
+    super(Bomb, self).__init__(0, 0, 'images/alien.png')
+    self.dy = 0
+    self.dx = (randint(0, 200) / 100) + 1
+    self.reset(lanes)
+
+  def move(self):
+    self.x += self.dx
+    self.y += self.dy
+
+    if self.x > 500 or self.x < -64 or self.y > 500 or self.y < -64:
+        self.randomize_directions()
+        self.reset(lanes)
+
+  def reset(self, lanes):
+      direction = self.randomize_directions()
+      if direction == 0:  # Moving horizontally
+          self.y = choice(lanes)
+          self.x = choice([-64, 500])
+      else:  # Moving vertically
+          self.x = randint(0, 500)  # Random x position within screen width
+          self.y = choice([-64, 500])
+
+  def randomize_directions(self):
+      direction = randint(0, 1)
+      if direction == 0:  # Move horizontally
+          self.dx = (randint(0, 200) / 100) + 1
+          self.dx *= choice([-1, 1])  # Randomly choose left or right
+          self.dy = 0
+      else:  # Move vertically
+          self.dy = (randint(0, 200) / 100) + 1
+          self.dy *= choice([-1, 1])  # Randomly choose up or down
+          self.dx = 0
+      return direction
 
 
 
@@ -111,6 +146,7 @@ class Player(GameObject):
 apple = Apple()
 strawberry = Strawberry()
 player = Player()
+bomb = Bomb()
 
 clock = pygame.time.Clock()
 
@@ -119,6 +155,7 @@ all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 all_sprites.add(apple)
 all_sprites.add(strawberry)
+all_sprites.add(bomb)
 
 
 running = True
